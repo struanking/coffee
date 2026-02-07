b440b1e326c9f7f32c9b0860dd4bfcc88f51470a
Struan King <struan.king@gmail.com>
2026-02-07 19:06:05 +0000

    Add tailwind (#1)
    * adds tailwind
    
    ---------
    
    Co-authored-by: Struan <struan@m3.local>

--- tina/schema/index.ts
+++
@@ -1,7 +0,0 @@
-import type { Schema } from 'tinacms';
-import { coffee } from './collections/coffee';
-import { supplier } from './collections/supplier';
-
-export const schema: Schema = {
-  collections: [coffee, supplier],
-};
