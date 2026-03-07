ecea1a791423378793a1b036163b03393dc8428a
Struan <struan.king@gmail.com>
2026-03-07 10:01:22 +0000

    add sugarcube css

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,5 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
+import { z } from 'astro/zod';
 import client from '../tina/__generated__/client';
 
 const coffee = defineCollection({
